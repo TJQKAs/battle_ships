cb3187d656a569f55dc364928f43c5b3c10228ca
Evgenii Leshchenko <A26052013@GMAIL.COM>
2015-09-09 15:47:34 +0100

    update

--- spec/ship_spec.rb
+++ spec/ship_spec.rb
@@ -1,4 +1,21 @@
 require 'ship'
+
 describe Ship do
-  
+  let(:ship){Ship.new('xx')}
+
+ it "records a hit to the ship" do
+   ship.receive_hit
+   expect(ship.number_of_hits.count).to eq(1) # better way is for array to reduce by 1
   end
+
+  it "knows its position" do
+   expect(ship.location).to eq('xx')
+  end
+
+  it "knows a ship that is size 2 has sunk" do
+   ship.receive_hit
+   expect{ship.receive_hit}.to raise_error("You sunk me!")
+  end
+
+
+end
