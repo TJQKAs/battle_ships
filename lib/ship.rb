a16317966a28ce1fff1e1422afa9142e9af7729a
Evgenii Leshchenko <A26052013@GMAIL.COM>
2015-09-09 15:49:05 +0100

    update

--- lib/ship.rb
+++ lib/ship.rb
@@ -1,8 +1,20 @@
 class Ship
-  
-  attr_accessor :position, :direction 
-  def initialize
-    @hits = 0
-    @size = 1
-  end 
+
+  attr_accessor :number_of_hits, :position
+
+
+  def initialize(coordinates)
+    @position = ''
+    @position << coordinates
+    @number_of_hits = ['X', 'X'] 
+
+  def location
+    return @position
+  end
+
+  def receive_hit
+    number_of_hits.pop
+    fail "You sunk me!" if number_of_hits.empty?
+  end
+
 end
