438e2e2b6dc5c6b29e1bdfc07a7845e1290b17a8
Evgenii Leshchenko <A26052013@GMAIL.COM>
2015-09-09 15:46:43 +0100

    update

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,18 +1,10 @@
 require 'player'
-describe Player do 
-  it 'Player can deploy ships and specify direction and position' do
-    p = Player.new
-    ship = Ship.new
-    p.place(ship, 'A1', :N)
-    expect(p.player_ships).to eq([ship])
-  end
+
+describe Player do
+
   
-  it 'Check whether our ship is hit' do
-    p = Player.new
-    ship = Ship.new
-    p.place(ship, 'A1', :N)
-    expect(p.receive_hit('A1')) == (:hit)
-end
+
+
 end
     
   
